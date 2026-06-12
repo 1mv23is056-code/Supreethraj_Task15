dae91c2037cc32733d9292a0b69decf0e483460d
1mv23is056-code <1mv23is056@sirmvit.edu>
2026-06-12 23:03:21 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -2,6 +2,10 @@ let array = [4,10,8,3,0,13,7,20];
 
 
 function maximum(arr) {
+    if (arr.length === 0) {
+        console.log("Array is empty.");
+        return null;
+    }
     let max = arr[0];
     for (let index = 1; index < arr.length; index++) {
         if (arr[index] > max) {
@@ -14,6 +18,10 @@ function maximum(arr) {
 console.log("Maximum value is: " + maximum(array));
 
 let summation = function(arr) {
+    if (arr.length === 0) {
+        console.log("Array is empty.");
+        return null;
+    }
     let sum = 0;
     for (let index = 0; index < arr.length; index++) {
         sum += arr[index]
@@ -25,9 +33,13 @@ let summation = function(arr) {
 console.log("Sum of all elements: "+ summation(array));
 
 let NoOfOdd = (arr) => {
+    if (arr.length === 0) {
+        console.log("Array is empty.");
+        return null;
+    }
     let count =0;
     for (let index = 0; index < arr.length; index++) {
-        if (arr[index]%2 === 0) {
+        if (arr[index]%2 !== 0) {
             count ++;
         }
         
