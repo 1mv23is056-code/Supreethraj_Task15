b60a8a3c5c0c951f4edee91ba3d3aab06b3d545a
1mv23is056-code <1mv23is056@sirmvit.edu>
2026-06-12 22:06:57 +0530

    task 15

---
+++ functions.js
@@ -0,0 +1,38 @@
+let array = [4,10,8,3,0,13,7,20];
+
+
+function maximum(arr) {
+    let max = arr[0];
+    for (let index = 1; index < arr.length; index++) {
+        if (arr[index] > max) {
+            max = arr[index];
+        }
+    }
+    return max;
+}
+
+console.log("Maximum value is: " + maximum(array));
+
+let summation = function(arr) {
+    let sum = 0;
+    for (let index = 0; index < arr.length; index++) {
+        sum += arr[index]
+        
+    }
+    return sum
+}
+
+console.log("Sum of all elements: "+ summation(array));
+
+let NoOfOdd = (arr) => {
+    let count =0;
+    for (let index = 0; index < arr.length; index++) {
+        if (arr[index]%2 === 0) {
+            count ++;
+        }
+        
+    }
+    return count;
+}
+
+console.log("Count of odd Numbers : "+NoOfOdd(array));
